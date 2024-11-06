498000b68d7b69a9aae430ee8c4a1ff5d14ba703
Nolhan OPPIO <nolhan.oppio@edu.esiee-it.fr>
2024-11-06 11:28:36 +0100

    add login & register

--- utils/common.php
+++ utils/common.php
@@ -1,13 +1,15 @@
 <?php
     require_once __DIR__. '/database.php';
     require_once __DIR__. '/userConnexion.php';
+    require_once __DIR__. '/security.php';
+
 
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
 }
 
 
-    $_SESSION['userId'] = '1';
+
 
 
 ?>
