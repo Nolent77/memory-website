129d53e5bb43efc6d640f66e08537102e6f78e38
Nayash <shayan.cahelo@edu.esiee-it.fr>
2024-11-06 16:06:29 +0100

    add verif user space

--- myAccount.php
+++ myAccount.php
@@ -2,25 +2,33 @@
 <html lang="fr">
 
 <?php
-$css_file = 'myAccount.css';
-include 'partials/head.php';
+    $css_file = 'myAccount.css';
+    include 'partials/head.php';
+
+    function isConnect() {
+        if ($_SESSION['userId'] == 7 ) {
+            header("Location: index.php");
+        }
+    }
+    isConnect();
 ?>
 
 <body>
-<?php include 'partials/header.php'; ?>
 <?php
-$userId = 1;
-$target_dir = "userfiles/";
-$target_file = $target_dir . $userId . '.jpg';
-$uploadOk = 1;
-if(isset($_POST["submit"])) {
-    if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+    include 'partials/header.php';
+
+    $userId = 1;
+    $target_dir = "userfiles/";
+    $target_file = $target_dir . $userId . '.jpg';
+    $uploadOk = 1;
+    if(isset($_POST["submit"])) {
+        if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
     }
-}
 
 ?>
 
