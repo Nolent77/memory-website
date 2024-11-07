cfb8a0e484616fa56ddce71e7bf8fb3cdc29d9bd
Nayash <shayan.cahelo@edu.esiee-it.fr>
2024-11-07 21:23:16 +0100

    fix link footer

--- partials/footer.php
+++ partials/footer.php
@@ -6,6 +6,7 @@
             <p><span>Téléphone : </span>07 83 70 28 44</p>
             <p><span>Email : </span>support@powerofmemory.fr</p>
             <p><span>Location : </span>Cergy</p>
+
         </div>
 
 
@@ -13,9 +14,10 @@
         <div class="footer-section">
             <div class="ul_fix_marge"><p>Power Of Memory</p></div>
             <ul class="footer-list">
-                <li>Jouer</li>
-                <li>Les scores</li>
-                <li>Nous contacter</li>
+                <a href="/games/memory/index.php"><li>Jouer</li></a>
+                <a href="/games/memory/scores.php"><li>Les scores</li></a>
+                <a href="/contact.php"><li>Nous contacter</li></a>
+
             </ul>
         </div>
 
